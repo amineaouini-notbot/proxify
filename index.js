bfba620e3795c0519420a3e84802878c93cecf1b
amine m. aouini <aminaouini565@gmail.com>
2023-10-14 15:23:52 +0100

    proxy internet to client

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var proxy = require('express-http-proxy');
 var app = require('express')();
 
-app.use('/', proxy('/'));
+app.use('/', proxy('/', {
+    userResDecorator: function(proxyRes, proxyResData, userReq, userRes) {
+      data = JSON.parse(proxyResData.toString('utf8'));
+      data.newProperty = 'exciting data';
+      return JSON.stringify(data);
+    }
+  }));
 
 app.listen(8080, ()=>{ console.log('Proxy is on!')})
